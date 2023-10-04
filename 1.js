3d85e3ffce075753f2389461c9bb6d10ad8cd1ce
yangjianchun <you@example.com>
2023-10-04 10:13:32 +0800

    feat

---
+++ 1.js
@@ -0,0 +1,6 @@
+var nums = [31,11,12,34,534,2,4,6,34,222,33,43,234]
+var newNums = []
+for (let index = 0; index < nums.length; index++) {
+    newNums.push(nums[index])
+}
+console.log(newNums,'lll')
